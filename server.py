90b0a1404bf7bba3fdd5bedc17970d6b55b655a9
Sammy <sammy@Sambartas-MacBook-Pro.local>
2019-11-30 04:51:20 +0530

    Setup contact page

--- server.py
+++ server.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def renderPage():
     return render_template("index.html")
 
+@app.route("/contact")
+def contactPage():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8000)
