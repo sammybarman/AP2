8d83737e7d291c8456f3c167af57085fdc02ffe3
Sammy <sammy@Sambartas-MacBook-Pro.local>
2019-11-30 04:40:02 +0530

    FrontEnd cleanup

--- static/js/init.js
+++ static/js/init.js
@@ -1,19 +1,4 @@
-$('.carousel.carousel-slider').carousel({
-    fullWidth: true,
-    indicators: true
-  });
-  var autoplay = true;
-  setInterval(function() { if(autoplay) $('.carousel.carousel-slider').carousel('next'); }, 2000);
-  $('.carousel.carousel-slider').hover(function(){ autoplay = false; },function(){ autoplay = true; });
-
 $(document).ready(function(){
-  $('.sidenav').sidenav();
-  $('.parallax').parallax();
-  $('.scrollspy').scrollSpy();
-  $('.materialboxed').materialbox();
-  $('.fixed-action-btn').floatingActionButton();
-  $('.tap-target').tapTarget();
-  $('.tooltipped').tooltip();
   $('.datepicker').datepicker({
     selectMonths: true,
     minDate: new Date(2019,10,29)
@@ -37,28 +22,3 @@ $(document).ready(function(){
 function getId() {
   alert($('#autocomplete').data('id'));
 }
-
-
-// $(window).scroll(function(){
-//
-//   if($(window).scrollTop()>20){
-//     $('nav').addClass('bg');
-//     $('.brand-logo').addClass('black-text');
-//     $('.forjq').addClass('bg2');
-//   }else{
-//     $('nav').removeClass('bg');
-//     $('.brand-logo').removeClass('black-text');
-//     $('.forjq').removeClass('bg2');
-//   }
-// });
-
-
-
-$(document).ready(function(){
-    $('.parallax').parallax();
-  });
-
-$(document).ready(function(){
-      $('.fixed-action-btn').floatingActionButton();
-
-  });
